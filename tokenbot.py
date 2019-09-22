881704344748ddd46686b83bce91946ff4fc323b
bonis955 <47026176+bonis955@users.noreply.github.com>
2019-09-22 17:51:40 +0200

    Add files via upload

--- tokenbot.py
+++ tokenbot.py
@@ -1,5 +1,6 @@
 DATABASE_URL="inserisci l'uri del database Postgressql"
 TOKEN="Inserisci il token del bot"
+id_bot="inserisci l'user id del bot"
 
 '''https://my.telegram.org/apps dovete creare una nuova app andando sul seguente sito,
    interite il vostro numero di telefono e create la app, Andate nelle impostazioni e 
