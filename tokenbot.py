cc94fd288b3c288e6b8108bf916ab764e61a3b76
bonis955 <47026176+bonis955@users.noreply.github.com>
2019-09-23 16:04:41 +0200

    Update tokenbot.py

--- tokenbot.py
+++ tokenbot.py
@@ -1,6 +1,5 @@
 DATABASE_URL="inserisci l'uri del database Postgressql"
 TOKEN="Inserisci il token del bot"
-id_bot="inserisci l'user id del bot"
 
 '''https://my.telegram.org/apps dovete creare una nuova app andando sul seguente sito,
    interite il vostro numero di telefono e create la app, Andate nelle impostazioni e 
@@ -8,3 +7,8 @@ id_bot="inserisci l'user id del bot"
 
 API_ID = 00000000000
 API_HASH = "ffffffffffff"
+
+#inserisci ivari dati del bot
+id_bot=000000000
+username_bot="@ciao"
+orario_script="00:00"
